06162b9c2361feded0ed7fcfc1fd60f7ea3c23f5
Carlos Gaona <carlosgaonad@gmail.com>
2023-12-28 17:25:51 -0500

    listar documentos y folders

---
+++ 02postman/create-report-groups.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Running create-report-groups.sh"
+
+for d in ./*/ ; do
+    # Loop through each subdirectory 
+    # and count the number of words in each file
+    (cd "$d" && wc -w *.xml);
+done
