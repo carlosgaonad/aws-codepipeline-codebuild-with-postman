22d96c4234346b873c79227e9374e07df1421c07
Carlos Gaona <carlosgaonad@gmail.com>
2023-12-29 11:00:11 -0500

    buscar dentro de un directorio el archivo xml

--- 02postman/create-report-groups.sh
+++ 02postman/create-report-groups.sh
@@ -6,5 +6,8 @@ for d in ./*/ ; do
     # Loop through each subdirectory 
     # and count the number of words in each file
     echo "List Directory $d"
-    (cd "$d" && ls -lrt);
+    for entry in "$d"/*
+    do
+        echo "File $entry on $d"
+    done
 done
