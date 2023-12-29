fc8cada3e92410150e22fa6c9d4c9ebb26a14613
Carlos Gaona <carlosgaonad@gmail.com>
2023-12-29 09:28:10 -0500

    modifying path of results

--- 02postman/create-report-groups.sh
+++ 02postman/create-report-groups.sh
@@ -5,5 +5,6 @@ echo "Running create-report-groups.sh"
 for d in ./*/ ; do
     # Loop through each subdirectory 
     # and count the number of words in each file
+    echo "List Directory $d"
     (cd "$d" && ls -lrt);
 done
