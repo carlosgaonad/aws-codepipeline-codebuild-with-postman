27e04cf9a99c7f6d4faa345ab03150f2da8f9457
Carlos Gaona <carlosgaonad@gmail.com>
2024-01-03 12:33:29 -0500

    Ajuste a nombres de los archivos

--- 02postman/create-report-groups.sh
+++ 02postman/create-report-groups.sh
@@ -9,13 +9,13 @@ for d in ./*/ ; do
     for entry in "$d"/*
     do
         echo "File $entry on $d"
-        REPORT_NAME=$(echo $d| rev |cut -d'/' -f 1)
+        REPORT_NAME=$(echo $d| cut -d'/' -f 2)
         echo " ******** $REPORT_NAME  ****"
         # ./02postman//newman-run-report-2024-01-02-21-57-59-022-0.xml
         # revert newman-run-report-2024-01-02-21-57-59-022-0.xml//02postman/.
-        FILE_REPORT=$(echo $entry| rev |cut -d'/' -f 1)
+        FILE_REPORT=$(echo $entry| rev |cut -d'/' -f 1 | rev)
         echo "XML File : $FILE_REPORT "
-        REPORT_ARN=$(aws codebuild create-report-group --name "ReportTest$REPORT_NAME" --type TEST --export-config reportGroupConf.json -query "reportGroup.{arn:arn}" | grep "arn" | tr -d '"')
+        REPORT_ARN=$(aws codebuild create-report-group --name "ReportTest$REPORT_NAME" --type TEST --export-config reportGroupConf.json --query "reportGroup.{arn:arn}" | grep "arn" | tr -d '"')
         echo "Report Group ReportTest$REPORT_NAME Created with ARN $REPORT_ARN "
     done
 done
