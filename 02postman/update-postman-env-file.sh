2daad4b862b756aff893449656f0cf94934a9b6f
Carlos Gaona <carlosgaonad@gmail.com>
2024-01-16 16:48:20 -0500

    add secret and session_token

--- 02postman/update-postman-env-file.sh
+++ 02postman/update-postman-env-file.sh
@@ -21,6 +21,16 @@ jq -e --arg AWS_ACCESS_KEY "$1" '(.values[] | select(.key=="AWS_ACCESS_KEY") | .
   && cp PetStoreAPIEnvironment.postman_environment.json.tmp PetStoreAPIEnvironment.postman_environment.json \
   && rm PetStoreAPIEnvironment.postman_environment.json.tmp
 
+jq -e --arg AWS_SECRET_KEY "$2" '(.values[] | select(.key=="AWS_SECRET_KEY") | .value) = $AWS_SECRET_KEY' \
+  PetStoreAPIEnvironment.postman_environment.json > PetStoreAPIEnvironment.postman_environment.json.tmp \
+  && cp PetStoreAPIEnvironment.postman_environment.json.tmp PetStoreAPIEnvironment.postman_environment.json \
+  && rm PetStoreAPIEnvironment.postman_environment.json.tmp
+
+jq -e --arg AWS_SESSION_TOKEN "$3" '(.values[] | select(.key=="AWS_SESSION_TOKEN") | .value) = $AWS_SESSION_TOKEN' \
+  PetStoreAPIEnvironment.postman_environment.json > PetStoreAPIEnvironment.postman_environment.json.tmp \
+  && cp PetStoreAPIEnvironment.postman_environment.json.tmp PetStoreAPIEnvironment.postman_environment.json \
+  && rm PetStoreAPIEnvironment.postman_environment.json.tmp
+
 echo "Updated PetStoreAPIEnvironment.postman_environment.json"
 
 cat PetStoreAPIEnvironment.postman_environment.json
