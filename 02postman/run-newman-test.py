1b45b72f24d6db60002d230af0d86a3a850793b6
Carlos Gaona <carlosgaonad@gmail.com>
2024-01-26 09:26:59 -0500

    Add python to read cdk json

---
+++ 02postman/run-newman-test.py
@@ -0,0 +1,22 @@
+import sys
+import json
+
+def main():
+    args = sys.argv[1:] #Stating by 0
+    servicesapi = []
+
+    with open("../cdk.json","r") as file:
+        jsonData = json.load(file)
+        #print(jsonData)
+        #print(jsonData['context']['Service']['resources'])
+        servicesapi = [x for x in jsonData['context']['Service']['resources']]
+    
+    #print(len(servicesapi))
+    for indexval in servicesapi:
+        #print(indexval['lambda']['image'])
+        lambdatid = indexval['lambda']['image']['id']
+        lambdatag = indexval['lambda']['image']['tag']
+        print("URL : "+ args[0]+"/"+args[1]+"/"+args[2]+"/"+args[3]+"/"+lambdatid+"/"+lambdatag)
+
+if __name__ == "__main__":
+    main()
