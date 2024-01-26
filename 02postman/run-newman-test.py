a337690ce25d935f7c54f1cd1c904124cd567760
Carlos Gaona <carlosgaonad@gmail.com>
2024-01-26 12:48:10 -0500

    add newman folders to store results

--- 02postman/run-newman-test.py
+++ 02postman/run-newman-test.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import json
+import subprocess
+
 
 def main():
     args = sys.argv[1:] #Stating by 0
@@ -21,6 +23,15 @@ def main():
         path = args[0]+"/"+args[1]+"/"+args[2]+"/"+args[3]+"/"+lambdatid+"/"+lambdatag
         isExist = os.path.exists(path)
         print("URL : "+ args[0]+"/"+args[1]+"/"+args[2]+"/"+args[3]+"/"+lambdatid+"/"+lambdatag + "   Exist="+str(isExist))
+        
+        if isExist :
+            #Crear directorio newman/servicio
+            tempnewmanurl = "newman/"+args[2]+"/"+lambdatid+"/"+lambdatag
+            subprocess.run(["mkdir", tempnewmanurl])
+            #command = "newman run postman_collection.json --environment postman_environment.json -r junit --reporter-junit-export " + tempnewmanurl
+            #process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+            #process.wait()
+
 
 if __name__ == "__main__":
     main()
