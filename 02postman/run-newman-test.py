aed02d6bbaf5bb1749ca588283308ee2c94adfc3
Carlos Gaona <carlosgaonad@gmail.com>
2024-01-26 16:55:01 -0500

    Changing commands newman 5pm

--- 02postman/run-newman-test.py
+++ 02postman/run-newman-test.py
@@ -36,11 +36,11 @@ def main():
             tempnewmanurl = "newman/"+args[2]+"/"+lambdatid
             subprocess.run(["mkdir", tempnewmanurl])
 
-            command = "newman run "+ pathCollection +" --environment "+pathEnvironment+" -r junit --reporter-junit-export " + tempnewmanurl
-            print("Ready to run command = "+ command)
-            subprocess.run(["newman", "run",pathCollection, "--environment", pathEnvironment , "-r", "junit", "--reporter-junit-export", tempnewmanurl ],
-                           shell=True, capture_output=True)
-            
+            #command = "newman run "+ pathCollection +" --environment "+pathEnvironment+" -r junit --reporter-junit-export " + tempnewmanurl
+            #print("Ready to run command = "+ command)
+            result = subprocess.run(["newman", "run",pathCollection, "--environment", pathEnvironment , "-r", "junit", "--reporter-junit-export", tempnewmanurl ],
+                           shell=True, capture_output=True, text=True)
+            print(result)
             #process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
             #process.wait()
         else:
